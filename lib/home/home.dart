f270863e0b3bf1398e858029759ec1a278284753
yamidduarte <yamid9@gmail.com>
2022-09-20 02:55:03 -0700

    Last before Google

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutly/shared/bottom_nav.dart';
+import 'package:flutly/login/login.dart';
+import 'package:flutly/topics/topics.dart';
+import 'package:flutly/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading'); //LoadingScreen();
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'), //ErrorMessage(),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
