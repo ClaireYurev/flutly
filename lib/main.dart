658fc72b348d389cf8c8652a512a5bd8639be390
yamidduarte <yamid9@gmail.com>
2022-09-19 19:18:53 -0700

    Latest dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   clairesFirebaseInitializer();
-}
-
-void clairesFirebaseInitializer() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -119,4 +114,10 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Future<void> clairesFirebaseInitializer() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
